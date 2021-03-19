f23bad6a387edabf04f23621719ea2d0229946ca
Frank Garcia <garcia1901l@gmail.com>
2021-03-19 18:18:58 -0500

    Install Laravel-Reaction package

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Qirolab\Laravel\Reactions\Traits\Reactable;
+use Qirolab\Laravel\Reactions\Contracts\ReactableInterface;
 
-class Post extends Model
+class Post extends Model implements ReactableInterface
 {
-    use HasFactory;
+    use HasFactory, Reactable;
 }
